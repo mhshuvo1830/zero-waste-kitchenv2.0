fc709fb7aad6929de5e4fce11f37b797ce3cae62
mhshuvo1830 <146349450+mhshuvo1830@users.noreply.github.com>
2026-08-12 11:19:41 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // PantryLoop Firebase Web App configuration
 // Connected to Firebase project: zerowaste2-42fda
 // Firebase Web config is client-side project configuration. Never add Admin SDK/service-account private keys here.
-export const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyAxfJFOfTQ7sA4w4Bsbz6cMTD4Z_9iljeU",
   authDomain: "zerowaste2-42fda.firebaseapp.com",
   projectId: "zerowaste2-42fda",
@@ -10,3 +10,5 @@ export const firebaseConfig = {
   appId: "1:155956475249:web:3de0fe9560da3ebf93f2d0",
   measurementId: "G-0ES10N6LPE"
 };
+
+window.__pantryloopFirebaseConfigLoaded = true;
